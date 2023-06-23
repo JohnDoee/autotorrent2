6e05fbec4ba9a3c0417f6a6c85521de48284b6b4
kannibalox <kannibalox@gmail.com>
2023-06-23 17:54:34 -0400

    Add tinycron

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ ENV PATH=/app/autotorrent/bin/:$PATH
 ENV HOME=/app/autotorrent
 WORKDIR /app/autotorrent
 
+RUN curl -sSL https://github.com/bcicen/tinycron/releases/download/v0.4/tinycron-0.4-linux-amd64 > /usr/local/bin/tinycron && chmod +x /usr/local/bin/tinycron
+
 COPY ./docker-entrypoint.sh /opt/docker-entrypoint.sh
 ENTRYPOINT ["/opt/docker-entrypoint.sh"]
 COPY . /tmp/autotorrent/
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,19 @@
 if [[ -n "$PUID" && -n "$PGID" ]]; then
   groupadd -g "$PGID" autotorrent
   useradd -u "$PUID" -g "$PGID" -M -d /app/autotorrent autotorrent
-  echo at2 "$@" | su autotorrent
+  if [[ "$1" == "cron" ]]; then
+    SCHEDULE="$2"
+    shift 2
+    echo /usr/local/bin/tinycron "$SCHEDULE" at2 "$@" | su autotorrent
+  else
+    echo at2 "$@" | su autotorrent
+  fi
 else
-  at2 "$@"
+  if [[ "$1" == "cron" ]]; then
+    SCHEDULE="$2"
+    shift 2
+    /usr/local/bin/tinycron "$SCHEDULE" at2 "$@"
+  else
+    at2 "$@"
+  fi
 fi
